ef1a8b9cfa5ce6ead5e8dbecaad601abf225a33f
Darnell <darnellwillis96@gmail.com>
2018-04-18 13:09:43 -0700

    new york times API added as well as assiocate links

--- apps/mypeckyspace/static/mypeckyspace/js/api.js
+++ apps/mypeckyspace/static/mypeckyspace/js/api.js
@@ -1,12 +1,18 @@
-var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+// Built by LucyBot. www.lucybot.com
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
   url += '?' + $.param({
-    'api-key': "cfd0871be48648f988de8c033e7cd57d"
+    'api-key': "cfd0871be48648f988de8c033e7cd57d",
+    'q': "amazon"
   });
   $.ajax({
     url: url,
     method: 'GET',
   }).done(function(result) {
     console.log(result);
+    for (var i = 1; i <= result.response.docs.length; i++) {
+      // document.getElementById('news').innerHTML = i;
+      $('#url').append('<a href=' + result.response.docs[i].web_url + '>' + result.response.docs[i].headline.print_headline + '</a>' + '<br>');
+    }
   }).fail(function(err) {
     throw err;
 });
